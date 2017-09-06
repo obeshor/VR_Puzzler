6915bc3b427c9e9d7cddbcff48aa7589c13c24e5
Yannick Serge <yannicksergeobam@gmail.com>
2017-09-06 12:38:23 +0100

    Add unity files

---
+++ VR-Puzzler-Project/Assets/Scripts/ExhibitAction.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExhibitAction : MonoBehaviour {
+
+    public GameObject photo;
+    private GvrAudioSource _audio_source;
+
+    // Use this for initialization
+    void Start () {
+        _audio_source = gameObject.GetComponent<GvrAudioSource>();
+        _audio_source.playOnAwake = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void popupPhoto() {
+        photo.SetActive(true);
+        _audio_source.Play();
+    }
+
+    public void closePopup()
+    {
+        photo.SetActive(false);
+        _audio_source.Play();
+    }
+}
